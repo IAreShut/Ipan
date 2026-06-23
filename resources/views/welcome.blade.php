82cdf0fb9a54d9b8d6e80ebf39a4d8498008b7a9
Irfan Arsyad <2024692246@student.uitm.edu.my>
2026-06-23 17:30:44 +0800

    fix css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.master')
 
 @push('styles')
-<link rel="stylesheet" href="{{ asset('css/index.css') }}?v={{ filemtime(public_path('css/index.css')) }}">
+<!-- <link rel="stylesheet" href="{{ asset('css/index.css') }}?v={{ filemtime(public_path('css/index.css')) }}"> -->
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endpush
 
 @section('content')
